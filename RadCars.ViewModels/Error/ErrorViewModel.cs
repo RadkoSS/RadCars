67a4f41448c0d9e089491e62f1055352d84b9535
Radoslav Gervaziev <radi7275@gmail.com>
2023-06-22 11:53:18 +0300

    Add project files.

---
+++ RadCars.ViewModels/Error/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RadCars.ViewModels.Error
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
