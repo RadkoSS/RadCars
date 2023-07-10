21dada560ceabc573ce3a72c7d269bb9d0482261
Radoslav Gervaziev <radi7275@gmail.com>
2023-07-10 11:14:39 +0300

    JS responsible for dynamically loading the uploaded images when creating a listing is succsessfully creating a carousel with the uploaded images. Refactored the _ListingFormPartial because it will be reused for Listing edit.

---
+++ RadCars.Web/wwwroot/js/previewUploadedImages.js
@@ -0,0 +1,40 @@
+﻿const input = document.getElementById('images');
+const removeImagesBtn = document.getElementById('clearImages');
+const carouselContainer = document.querySelector('.carousel-inner');
+const carouselWrap = document.getElementById('carouselExampleControls');
+
+input.addEventListener('change', () => {
+    carouselContainer.innerHTML = '';
+
+    for (let i = 0; i < input.files.length; i++) {
+
+        const carouselItem = document.createElement('div');
+        if (i === 0) {
+            carouselItem.className = 'carousel-item active';
+        } else {
+            carouselItem.className = 'carousel-item';
+        }
+
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(input.files[i]);
+        img.className = 'd-block object-fit-cover w-100 h-100';
+        img.alt = 'Снимката не беше заредена...';
+
+        img.onload = () => {
+            URL.revokeObjectURL(this.src);
+        }
+
+        carouselItem.appendChild(img);
+        carouselContainer.appendChild(carouselItem);
+    }
+
+    carouselWrap.classList.remove('visually-hidden');
+    removeImagesBtn.classList.remove('visually-hidden');
+});
+
+removeImagesBtn.addEventListener('click', () => {
+    input.value = '';
+    carouselContainer.innerHTML = '';
+    carouselWrap.classList.add('visually-hidden');
+    removeImagesBtn.classList.add('visually-hidden');
+});
