698a46f181864a4ca420dddc0b8b89dacdac6b9f
Radoslav Gervaziev <radi7275@gmail.com>
2023-07-13 02:18:05 +0300

    Details view now has dynamic image viewer implemented with vanilla JS.

---
+++ RadCars.Web/wwwroot/js/imageViewer.js
@@ -0,0 +1,23 @@
+﻿let currentImageIndex = 0;
+const images = Array.from(document.querySelectorAll('[data-bs-img-url]')).map(a => a.getAttribute('data-bs-img-url'));
+
+document.querySelectorAll('[data-bs-toggle="modal"]').forEach((item, index) => {
+    item.addEventListener('click', event => {
+        // Button that triggered the modal
+        const imageUrl = event.currentTarget.getAttribute('data-bs-img-url');
+        // Update the modal's content.
+        const modalImage = document.querySelector('#modalImage');
+        modalImage.src = imageUrl;
+        currentImageIndex = index; // Update the current image index
+    });
+});
+
+document.querySelector('#prevBtn').addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    document.querySelector('#modalImage').src = images[currentImageIndex];
+});
+
+document.querySelector('#nextBtn').addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.querySelector('#modalImage').src = images[currentImageIndex];
+});
