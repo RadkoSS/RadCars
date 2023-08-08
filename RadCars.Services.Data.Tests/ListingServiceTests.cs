c596768b1651ce086fa23bf7095c1e3535a42abe
Radoslav Gervaziev <radi7275@gmail.com>
2023-08-08 10:35:14 +0300

    Added some changes to the Index view and created the Tests project

---
+++ RadCars.Services.Data.Tests/ListingServiceTests.cs
@@ -0,0 +1,15 @@
+namespace RadCars.Services.Data.Tests;
+
+public class ListingServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
